18017ab79b9049464190e0b1dcf3321b0145f4e1
sample279 <224307167+sample279@users.noreply.github.com>
2026-07-31 22:02:45 +0530

    feat: add dust alias and sort shell aliases

--- nixos-config/home/shell.nix
+++ nixos-config/home/shell.nix
@@ -9,20 +9,21 @@
     '';
 
     shellAliases = {
+      a = "alias | bat -l bash";
       btw = "echo i use nixos btw";
+      cfg = "cd ~/config";
+      dust = "dust -s -r -x";
+      emacs = "emacsclient -c & disown";
+      ff = "fzf --preview 'bat --style=numbers --color=always {}'";
+      ls = "eza -lh --group-directories-first --icons=auto";
+      ncg = "sudo nix-collect-garbage -d";
       nrs = "sudo nixos-rebuild switch --flake ~/config/nixos-config#sample";
       nrst = "sudo nixos-rebuild switch --flake ~/config/nixos-config#sample --show-trace";
-      ncg = "sudo nix-collect-garbage -d";
-      nso = "sudo nix store optimise";
       nru = "nix flake update ~/config/nixos-config && nrs";
       nrup= "nix flake update nixpkgs --flake ~/config/nixos-config && nrs";
-      ls = "eza -lh --group-directories-first --icons=auto";
-      ff = "fzf --preview 'bat --style=numbers --color=always {}'";
-      emacs = "emacsclient -c & disown";
+      nso = "sudo nix store optimise";
       nv = "nvim";
       pg = "pgadmin4";
-      a = "alias | bat -l bash";
-      cfg = "cd ~/config";
       stowall = "cd ~/config/dotfiles && stow -t ~ * && cd -";
     };
   };
