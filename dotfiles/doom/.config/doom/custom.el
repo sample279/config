0c911dacafd79b38546e13fe2b0598115860c9e7
sample279 <224307167+sample279@users.noreply.github.com>
2026-08-09 14:20:32 +0530

    chore: add doom custom safe themes

---
+++ dotfiles/doom/.config/doom/custom.el
@@ -0,0 +1,15 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("adfa37a05cf17c803bc33f58fa075677c2574a4fd337c6dc26ceacf6317bfa39"
+     "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
