716874d01157a963c0c76eb215fbd5e39b88dab4
sample279 <224307167+sample279@users.noreply.github.com>
2026-08-11 02:10:16 +0530

    feat: enable doom make and tmux modules

--- dotfiles/doom/.config/doom/init.el
+++ dotfiles/doom/.config/doom/init.el
@@ -103,11 +103,11 @@
        ;;llm               ; when I said you needed friends, I didn't mean...
        (lsp +eglot)      ; M-x vscode
        magit             ; a git porcelain for Emacs
-       ;;make              ; run make tasks from Emacs
+       make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        pdf               ; pdf enhancements
        ;;terraform         ; infrastructure as code
-       ;;tmux              ; an API for interacting with tmux
+       tmux              ; an API for interacting with tmux
        tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
 
