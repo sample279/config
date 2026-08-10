bf3eab5ebfc38fd40112bffea03421196aa4c202
sample279 <224307167+sample279@users.noreply.github.com>
2026-08-11 02:10:16 +0530

    chore: add doom custom safe theme

--- dotfiles/doom/.config/doom/custom.el
+++ dotfiles/doom/.config/doom/custom.el
@@ -5,7 +5,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("adfa37a05cf17c803bc33f58fa075677c2574a4fd337c6dc26ceacf6317bfa39"
+   '("72bdccf021499244dab0bd5a4edad3e028ef6751b71b0c84c518bd930411405d"
+     "adfa37a05cf17c803bc33f58fa075677c2574a4fd337c6dc26ceacf6317bfa39"
      "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
